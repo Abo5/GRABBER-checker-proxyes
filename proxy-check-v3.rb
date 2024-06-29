fa118de9fdc40a5c7364c6994d6e9568e934db82
Abo5 <40110835+Abo5@users.noreply.github.com>
2024-06-29 20:08:06 +0300

    Update proxy-check-v3.rb

--- proxy-check-v3.rb
+++ proxy-check-v3.rb
@@ -1,46 +1,52 @@
+# _librarys_
 require 'httparty'
 require 'json'
 require 'openssl'
-require 'parallel'
+require 'thread'
 
-# Check and install required gems
-gems_to_check = ['httparty', 'openssl', 'json', 'parallel']
-gems_to_check.each do |gem|
-  begin
-    gem_installed = Gem::Specification.find_by_name(gem)
-    puts "\e[38;2;0;255;0m[✓]\e[0m The gem #{gem} is installed "
-  rescue Gem::LoadError
-    puts "\e[31;1m[✗]\e[0m The gem #{gem} is not installed "
-    system("gem install #{gem}")
-    Gem.clear_paths
-  end
-end
-
-# Colors
-MAGENTA = "\e[38;2;128;0;128m"
-YELLOW = "\e[38;2;255;255;0m"
-ORANGE = "\e[38;2;255;165;0m"
-RESET = "\e[0m"
-GREEN = "\e[38;2;0;255;0m"
-RED = "\e[38;2;255;0;0m"
-PINK = "\e[38;2;255;105;180m"
-
-def running
-  print "#{ORANGE}Running"
-  3.times do
-    sleep(0.5)
-    print "."
-  end
-  puts RESET
-  sleep(0.5)
-end
-
-running
+# clear_in_terminal
 system("clear")
 
-# Logo
+gems_to_check = ['httparty', 'json', 'thread'] # array_of_gem
+gems_to_check.each do |gem|
+  gem_installed = Gem::Specification.find_by_name(gem) # check_if_gem_is_installed?
+  if gem_installed
+    puts "\e[38;2;0;255;0m[✓]\e[0m The gem #{gem} is installed "
+  else
+    puts "\e[31;1m[✗]\e[0m The gem #{gem} is not installed "
+    system("gem install #{gem}")
+    Gem.install(gem)
+  end
+end
+
+# _colors_
+magenta = "\e[38;2;128;0;128m" # "#{magenta}"
+yellow = "\e[38;2;255;255;0m" # "#{yellow}"
+orange = "\e[38;2;255;165;0m" # "#{orange}"
+nothing = "\e[0m"             # "#{nothing}"
+green = "\e[38;2;0;255;0m"    # "#{green}"
+red = "\e[38;2;255;0;0m"      # "#{red}"
+pink = "\e[38;2;255;105;180m" # "#{pink}"
+
+# print Running...
+def running
+  print "\e[38;2;255;165;0mRunning"
+  sleep(0.5)
+  print "."
+  sleep(0.5)
+  print "."
+  sleep(0.5)
+  puts ".\e[0m"
+  sleep(0.5)
+end
+running
+
+# clear_in_terminal
+system("clear")
+
+# printing_logo
 logo = <<LOGO
-#{MAGENTA}
+#{magenta}
 ..............................................
 ..............................................
 .............                .................
@@ -48,10 +54,10 @@ logo = <<LOGO
 ...........                          .........
 ..........                            ........
 .........                               ......
-.......:.          #{RESET}#{YELLOW}Proxy Scraper!#{RESET}#{MAGENTA}        .....
-......::           #{RESET}#{YELLOW}CoDe By A88#{RESET}#{MAGENTA}            ....
-.....:::.          #{RESET}#{YELLOW}Version 2.0#{RESET}#{MAGENTA}            ....
-....::::.          #{RESET}#{YELLOW}Recommended use vpn#{RESET}#{MAGENTA}     ...
+.......:.          #{nothing}#{yellow}Proxy Scraper!#{nothing}#{magenta}        .....
+......::           #{nothing}#{yellow}CoDe By A88#{nothing}#{magenta}            ....
+.....:::.          #{nothing}#{yellow}Version 2.0#{nothing}#{magenta}            ....
+....::::.          #{nothing}#{yellow}Recommended use vpn#{nothing}#{magenta}     ...
 ...::::::                                  ...
 ..::::::-.                                 ::.
 .::::::::-.                               .:::
@@ -64,19 +70,22 @@ logo = <<LOGO
 :----::...                       .:----------:
 :---:                           .  ...--------
 ::::.                            . .  ...::---
-#{RESET}
+#{nothing}
 LOGO
 
-logo.each_char { |c| print c; sleep(0.0000000001) }
+logo.each_char do |c|
+  print c
+  sleep(0.0000000001) # Random sleep delay up to 5 seconds
+end
 
-print "#{YELLOW}Hi, Welcome To Grabber And Checking Proxies, Write #{RESET}#{PINK}00#{RESET}#{YELLOW} To Exit\n"
+print "#{yellow}Hi, Welcome To Grabber And Checking Proxies, Write #{nothing}#{pink}00#{nothing}#{yellow} To Exit\n"
 print "1. Checker Proxies\n"
 print "2. Grabber Proxies\n"
 print "3. Grabber and Checker Proxies\n"
-print "Please enter your choice (1, 2, or 3): #{RESET}"
+print "Please enter your choice (1, 2, or 3): #{nothing}"
 choice = gets.chomp.to_i
 
-if choice == 0
+if choice == 00
   puts "Exiting...Thank You For Using Me"
   exit
 end
@@ -84,79 +93,110 @@ end
 $stop_grabbing_proxies = false
 Signal.trap("INT") do
   $stop_grabbing_proxies = true
-  puts "\n#{MAGENTA}[!]STOPPING...GRABBING...NOW!#{RESET}"
+  puts "\n\e[0m\e[38;2;128;0;128m[!]STOPPING...GRABBING...NOW!\e[0m"
 end
 
 def grab_proxies(source_url)
   return if $stop_grabbing_proxies
+
   return unless source_url.start_with?("https://")
 
   begin
     response = HTTParty.get(source_url, timeout: 3, verify: false)
     handle_response(response)
-  rescue StandardError => e
+  rescue OpenSSL::SSL::SSLError => e
+    puts "SSL Error - Skipping source URL: #{source_url}"
+  rescue Errno::ECONNRESET => e
+    puts "Connection reset by peer - Skipping source URL: #{source_url}"
+  rescue => e
     puts "Error: #{e.message} - Skipping source URL: #{source_url}"
   end
 end
 
 def handle_response(response)
-  if response && response.code == 200
-    proxies = response.body.scan(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+/)
-    if proxies.any?
-      proxies.each { |proxy| puts proxy }
-      File.open("proxy.txt", "a") do |file|
-        proxies.each { |proxy| file.puts proxy }
+  if response
+    if response.code == 200
+      body = response.body
+      proxies = body.scan(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+/) # Searching for the specified format in the page
+      if proxies.any?
+        proxies.each { |proxy| puts proxy }
+        proxy_list = File.open(File.join(__dir__, "proxy.txt"), "a") do |file|
+          proxies.each { |proxy| file.puts proxy }
+        end
+      else
+        puts "No proxies found in the page"
       end
     else
-      puts "No proxies found in the page"
+      puts "Failed to get proxies (HTTP Status: #{response.code})"
     end
   else
-    puts "Failed to get proxies (HTTP Status: #{response&.code || 'Unknown'})"
+    puts "No response received within the specified timeout."
   end
 end
 
 if choice == 2 || choice == 3
-  puts "#{ORANGE}[!] Grabbing Proxies...\n"
-  file_sources = File.readlines('sources.txt', chomp: true)
-  Parallel.each(file_sources, in_threads: 20) do |source_url|
+  puts "#{orange}[!] Grabbing Proxies...\n"
+
+  file_sources = File.readlines(File.join(__dir__, 'sources.txt'), chomp: true)
+  file_sources.each do |source_url|
     grab_proxies(source_url)
   end
 end
 
 if choice == 1 || choice == 3
-  puts "\n#{MAGENTA}[!] Grabbing Proxies Is Done, Now Checking#{RESET}"
+  puts "\n\e[38;2;128;0;128m[!] Grabbing Proxies Is Done, Now Checking"
+  Signal.trap("INT") do
+    puts "\nExiting...Thank You For Using Me"
+    exit
+  end
 
-  proxies = File.readlines('proxy.txt', chomp: true).uniq
-  working_proxies = Parallel.map(proxies, in_threads: 100) do |proxy|
-    proxy_host, proxy_port = proxy.split(':')
-    begin
-      response = HTTParty.get('http://www.google.com', http_proxyaddr: proxy_host, http_proxyport: proxy_port, timeout: 5)
-      if response.code == 200
-        puts "#{GREEN}Working on HTTP ----> #{proxy}#{RESET}"
-        proxy
-      else
-        response_https = HTTParty.get('https://www.google.com', http_proxyaddr: proxy_host, http_proxyport: proxy_port, timeout: 5, verify: false)
-        if response_https.code == 200
-          puts "#{GREEN}Working on HTTPS ----> #{proxy}#{RESET}"
-          proxy
-        else
-          puts "#{PINK}#{proxy} not working. HTTP code: #{response.code}, HTTPS code: #{response_https.code}#{RESET}"
-          nil
+  proxies = File.readlines(File.join(__dir__, 'proxy.txt'), chomp: true)
+  
+  thread_count = 50 # يمكنك ضبط هذا العدد حسب أداء جهازك
+  mutex = Mutex.new
+  threads = []
+
+  proxies.each_slice(proxies.size / thread_count + 1) do |proxy_slice|
+    threads << Thread.new do
+      proxy_slice.each do |proxy|
+        proxy_host, proxy_port = proxy.split(':')
+        begin
+          response = HTTParty.get('http://www.google.com', http_proxyaddr: proxy_host, http_proxyport: proxy_port, timeout: 0.5)
+          if response.code == 200
+            mutex.synchronize do
+              puts "\e[38;2;144;238;144mWorking on HTTP ----> #{proxy_host}:#{proxy_port}\e[0m"
+              File.open(File.join(__dir__, 'Hits-proxy.txt'), 'a') { |file| file.puts "#{proxy_host}:#{proxy_port}" }
+            end
+          else
+            response_https = HTTParty.get('https://www.google.com', http_proxyaddr: proxy_host, http_proxyport: proxy_port, timeout: 0.5, verify: OpenSSL::SSL::VERIFY_NONE)
+            if response_https.code == 200
+              mutex.synchronize do
+                puts "\e[38;2;144;238;144mWorking on HTTPS ----> #{proxy_host}:#{proxy_port}\e[0m"
+                File.open(File.join(__dir__, 'Hits-proxy.txt'), 'a') { |file| file.puts "#{proxy_host}:#{proxy_port}" }
+              end
+            else
+              mutex.synchronize do
+                puts "\e[38;2;255;105;180m#{proxy} not working. HTTP code: #{response.code}, HTTPS code: #{response_https.code}\e[0m"
+                proxies.delete(proxy)
+              end
+            end
+          end
+        rescue StandardError => e
+          mutex.synchronize do
+            puts "\e[38;2;255;165;0m#{proxy} not working. #{e.message}\e[0m"
+            proxies.delete(proxy)
+          end
         end
       end
-    rescue StandardError => e
-      puts "#{ORANGE}#{proxy} not working. #{e.message}#{RESET}"
-      nil
     end
-  end.compact
-
-  File.open('Hits-proxy.txt', 'w') do |file|
-    working_proxies.each { |proxy| file.puts proxy }
   end
 
-  File.open('proxy.txt', 'w') do |file|
-    working_proxies.each { |proxy| file.puts proxy }
-  end
+  threads.each(&:join)
 
-  puts "#{GREEN}Found #{working_proxies.size} working proxies out of #{proxies.size}#{RESET}"
+  # إعادة كتابة الملف proxy.txt بعد حذف البروكسيات غير الصالحة
+  File.open(File.join(__dir__, 'proxy.txt'), 'w') do |file|
+    proxies.each { |p| file.puts(p) }
+  end
 end
+
+puts "\n\e[38;2;128;0;128m[!] Process completed. Thank you for using the script!\e[0m"
